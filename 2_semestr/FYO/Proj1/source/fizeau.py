0de98441b32c22d6d2a70664fa791875cdd181e5
LachubCz <petr.genius.buchal@gmail.com>
2019-03-22 22:56:12 +0100

    Good animation resolution.

--- 2_semestr/FYO/Proj1/source/fizeau.py
+++ 2_semestr/FYO/Proj1/source/fizeau.py
@@ -7,12 +7,12 @@ from timeit import default_timer as timer
 from tools import exp_type, overlay_image_alpha
 
 
-def get_60(fizeau_default, fizeau_sprocket):
+def get_120(fizeau_default, fizeau_sprocket):
     array = deque([])
     (h, w) = fizeau_sprocket.shape[:2]
     center = (w / 2, h / 2)
 
-    for e in range(4):
+    for e in range(8):
         for i in range(15):
             M = cv2.getRotationMatrix2D(center, 2*i, 1.0)
             temp = cv2.warpAffine(fizeau_sprocket, M, (h, w))
@@ -32,20 +32,21 @@ def put_line(sprites, fizeau_dot):
 
     y_high = 249
     for i, item in enumerate(sprites):
-        if i < 30:
-            overlay_image_alpha(item, fizeau_dot[:, :, 0:3], (int(x+i*32.67), y), fizeau_dot[:, :, 3] / 255.0)
+        if i < 62:
+            print("1: ", i, " - ", int(x+i*16.335))
+            overlay_image_alpha(item, fizeau_dot[:, :, 0:3], (int(x+i*16.335), y), fizeau_dot[:, :, 3] / 255.0)
             sprites[i] = item
-        elif i == 30:
+        elif i == 62:
+            print("2: ", i, " - ", x_right)
             overlay_image_alpha(item, fizeau_dot[:, :, 0:3], (x_right, y), fizeau_dot[:, :, 3] / 255.0)
             sprites[i] = item
-        elif i > 32 and i < 50:
-            overlay_image_alpha(item, fizeau_dot[:, :, 0:3], (int(x_right-(i-30)*32.67), y), fizeau_dot[:, :, 3] / 255.0)
+        elif i > 62 and i < 102:
+            print("3: ", i, " - ", int(x_right-(i-62)*16.335))
+            overlay_image_alpha(item, fizeau_dot[:, :, 0:3], (int(x_right-(i-62)*16.335), y), fizeau_dot[:, :, 3] / 255.0)
             sprites[i] = item
-        elif i > 48 and i < 58:
-            overlay_image_alpha(item, fizeau_dot[:, :, 0:3], (shift, int(y-(i-50)*32.67)), fizeau_dot[:, :, 3] / 255.0)
-            sprites[i] = item
-        elif i == 58:
-            overlay_image_alpha(item, fizeau_dot[:, :, 0:3], (shift, y_high), fizeau_dot[:, :, 3] / 255.0)
+        elif i > 101 and i < 118:
+            print("4: ", i, " - ", int(y-(i-102)*16.335))
+            overlay_image_alpha(item, fizeau_dot[:, :, 0:3], (shift, int(y-(i-102)*16.335)), fizeau_dot[:, :, 3] / 255.0)
             sprites[i] = item
     return sprites
 
@@ -58,25 +59,25 @@ def fizeau(frame, expr):
     fizeau_line = cv2.imread("images/fizeau_line.png", 1)
     fizeau_dot = cv2.imread("images/fizeau_dot.png", cv2.IMREAD_UNCHANGED)
     fizeau_sprocket = cv2.imread("images/fizeau_sprocket.png", cv2.IMREAD_UNCHANGED)
-    wheel_sprites = get_60(fizeau_default, fizeau_sprocket)
-    wheel_sprites = put_line(wheel_sprites, fizeau_dot)
-    wheel_sprites.rotate(15)
-    wheel_sprites = put_line(wheel_sprites, fizeau_dot)
-    wheel_sprites.rotate(15)
-    wheel_sprites = put_line(wheel_sprites, fizeau_dot)
-    wheel_sprites.rotate(15)
-    wheel_sprites = put_line(wheel_sprites, fizeau_dot)
-    
+    wheel_sprites_pos = get_120(fizeau_default, fizeau_sprocket)
+    wheel_sprites_pos = put_line(wheel_sprites_pos, fizeau_dot)
+    wheel_sprites_pos.rotate(30)
+    wheel_sprites_pos = put_line(wheel_sprites_pos, fizeau_dot)
+    wheel_sprites_pos.rotate(30)
+    wheel_sprites_pos = put_line(wheel_sprites_pos, fizeau_dot)
+    wheel_sprites_pos.rotate(30)
+    wheel_sprites_pos = put_line(wheel_sprites_pos, fizeau_dot)
+
     animation = 0
     frequency_tr = [1.1]
 
     while (True):
         frame[:] = (49,52,49)
         #Animation window
-        cvui.image(frame, 0, 0, wheel_sprites[animation])
+        cvui.image(frame, 0, 0, wheel_sprites_pos[animation])
         cvui.image(frame, 872, 499, fizeau_line)
         animation += 1
-        if animation == 60:
+        if animation == 120:
             animation = 0
 
         #cvui.image(frame, 400, 400, fizeau_sprocket)
@@ -88,7 +89,7 @@ def fizeau(frame, expr):
         cvui.checkbox(frame, 10, 99, "1879 - Michelson", experiments[3])
 
         #Experiment settings window
-        cvui.window(frame, 1033.5, 2, 243.5, 133, 'Experiment settings')
+        cvui.window(frame, 1033.5, 2, 243.5, 104, 'Experiment settings')
         
         cvui.trackbar(frame,  1030, 39, 249, frequency_tr, 1.1, 10.0)
         cvui.rect(frame, 1035, 39, 240, 12, 0x313131, 0x313131)
