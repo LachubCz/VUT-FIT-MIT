cdad324f4ee01a92d967e3c6c46bc820fd831575
LachubCz <petr.genius.buchal@gmail.com>
2019-04-17 14:16:00 +0200

    ZPO

--- 2_semestr/ZPO/Proj6/main.py
+++ 2_semestr/ZPO/Proj6/main.py
@@ -91,7 +91,8 @@ def conv_layers_VGG_16(weights_path=None, pop_useless=True):
 
 
 class RoiPoolingConv(Layer):
-    '''ROI pooling layer for 2D inputs.
+    '''
+    ROI pooling layer for 2D inputs.
     See Spatial Pyramid Pooling in Deep Convolutional Networks for Visual Recognition,
     K. He, X. Zhang, S. Ren, J. Sun
     # Arguments
@@ -101,32 +102,47 @@ class RoiPoolingConv(Layer):
     # Input shape
         list of two 4D tensors [X_img,X_roi] with shape:
         X_img:
-        4D tensor with shape:
-        `(1, rows, cols, channels)`.
+        `(1, channels, rows, cols)` if dim_ordering='th'
+        or 4D tensor with shape:
+        `(1, rows, cols, channels)` if dim_ordering='tf'.
         X_roi:
-        `(1, num_rois, 4)` list of rois, with ordering (x, y, w, h)
+        `(1,num_rois,4)` list of rois, with ordering (x,y,w,h)
     # Output shape
-        A tensor with shape:
-        `(1, num_rois, pool_size, pool_size, channels)`
+        3D tensor with shape:
+        `(1, num_rois, channels, pool_size, pool_size)`
     '''
+
     def __init__(self, pool_size, num_rois, **kwargs):
+
+        self.dim_ordering = K.image_dim_ordering()
+        assert self.dim_ordering in {'tf', 'th'}, 'dim_ordering must be in {tf, th}'
+
         self.pool_size = pool_size
         self.num_rois = num_rois
+
         super(RoiPoolingConv, self).__init__(**kwargs)
 
-    
     def build(self, input_shape):
-        self.nb_channels = input_shape[0][3]
+        if self.dim_ordering == 'th':
+            self.nb_channels = input_shape[0][1]
+        elif self.dim_ordering == 'tf':
+            self.nb_channels = input_shape[0][3]
 
-    
     def compute_output_shape(self, input_shape):
-        return None, self.num_rois, self.pool_size, self.pool_size, self.nb_channels
+        if self.dim_ordering == 'th':
+            return None, self.num_rois, self.nb_channels, self.pool_size, self.pool_size
+        else:
+            return None, self.num_rois, self.pool_size, self.pool_size, self.nb_channels
 
     def call(self, x, mask=None):
+
         assert(len(x) == 2)
+
         img = x[0]
         rois = x[1]
+
         input_shape = K.shape(img)
+
         outputs = []
 
         for roi_idx in range(self.num_rois):
@@ -136,31 +152,56 @@ class RoiPoolingConv(Layer):
             w = rois[0, roi_idx, 2]
             h = rois[0, roi_idx, 3]
 
-            x = K.cast(x, 'int32')
-            y = K.cast(y, 'int32')
-            w = K.cast(w, 'int32')
-            h = K.cast(h, 'int32')
-            rs = tf.image.resize_images(
-                img[:, y:y+h, x:x+w, :], 
-                (self.pool_size, self.pool_size)
-            )
-            outputs.append(rs)
+            row_length = w / float(self.pool_size)
+            col_length = h / float(self.pool_size)
+
+            num_pool_regions = self.pool_size
+
+            #NOTE: the RoiPooling implementation differs between theano and tensorflow due to the lack of a resize op
+            # in theano. The theano implementation is much less efficient and leads to long compile times
+
+            if self.dim_ordering == 'th':
+                for jy in range(num_pool_regions):
+                    for ix in range(num_pool_regions):
+                        x1 = x + ix * row_length
+                        x2 = x1 + row_length
+                        y1 = y + jy * col_length
+                        y2 = y1 + col_length
+
+                        x1 = K.cast(x1, 'int32')
+                        x2 = K.cast(x2, 'int32')
+                        y1 = K.cast(y1, 'int32')
+                        y2 = K.cast(y2, 'int32')
+
+                        x2 = x1 + K.maximum(1,x2-x1)
+                        y2 = y1 + K.maximum(1,y2-y1)
+
+                        new_shape = [input_shape[0], input_shape[1],
+                                     y2 - y1, x2 - x1]
+
+                        x_crop = img[:, :, y1:y2, x1:x2]
+                        xm = K.reshape(x_crop, new_shape)
+                        pooled_val = K.max(xm, axis=(2, 3))
+                        outputs.append(pooled_val)
+
+            elif self.dim_ordering == 'tf':
+                x = K.cast(x, 'int32')
+                y = K.cast(y, 'int32')
+                w = K.cast(w, 'int32')
+                h = K.cast(h, 'int32')
+
+                rs = tf.image.resize_images(img[:, y:y+h, x:x+w, :], (self.pool_size, self.pool_size))
+                outputs.append(rs)
 
         final_output = K.concatenate(outputs, axis=0)
         final_output = K.reshape(final_output, (1, self.num_rois, self.pool_size, self.pool_size, self.nb_channels))
 
-        final_output = K.permute_dimensions(final_output, (0, 1, 2, 3, 4))
+        if self.dim_ordering == 'th':
+            final_output = K.permute_dimensions(final_output, (0, 1, 4, 2, 3))
+        else:
+            final_output = K.permute_dimensions(final_output, (0, 1, 2, 3, 4))
 
         return final_output
-    
-    
-    def get_config(self):
-        config = {
-            'pool_size': self.pool_size,
-            'num_rois': self.num_rois
-        }
-        base_config = super(RoiPoolingConv, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
 
 
 def SRM(imgs):
@@ -287,7 +328,8 @@ def network():
     #########
     #API model
     input_ = Input(batch_shape=rgb_conv_layers.output_shape)
-    x = Dense(256, activation='relu')(input_)
+    x = RoiPoolingConv(pool_size=8, num_rois=1)(input_)
+    x = Dense(256, activation='relu')(x)
     x = Dropout(0.5)(x)
     predict = Dense(1, activation='sigmoid')(x)
     api_model = Model(input_, predict)
@@ -304,7 +346,7 @@ def network():
     #########
 
 if __name__ == "__main__":
-    main()
+    network()
     """
     input_ = SRM(x)
 
