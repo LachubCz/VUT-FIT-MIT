5ec649c3bc638791d17113f8c3cb85b0d8d5d5c4
LachubCz <petr.genius.buchal@gmail.com>
2019-12-14 10:37:18 +0100

    Changes GAL.

--- 3_semestr/GAL/Proj1/graph.py
+++ 3_semestr/GAL/Proj1/graph.py
@@ -1,100 +1,76 @@
-""" A Python Class
-A simple Python graph class, demonstrating the essential 
-facts and functionalities of graphs.
-"""
-
 class Graph(object):
     def __init__(self, graph_dict=None):
-        """ initializes a graph object 
-            If no dictionary or None is given, 
-            an empty dictionary will be used
-        """
         if graph_dict == None:
             graph_dict = {}
-        self.__graph_dict = graph_dict
+        self.graph_dict = graph_dict
 
-    def vertices(self):
-        """ returns the vertices of a graph """
-        return list(self.__graph_dict.keys())
+    def get_vertices(self):
+        return list(self.graph_dict.keys())
 
-    def edges(self):
-        """ returns the edges of a graph """
+    def get_edges(self):
         return self.__generate_edges()
 
+    def get_graph_dict(self):
+        return self.graph_dict
+
+    def get_Adj(self, vertex):
+        return self.graph_dict[vertex]
+
     def add_vertex(self, vertex):
-        """ If the vertex "vertex" is not in 
-            self.__graph_dict, a key "vertex" with an empty
-            list as a value is added to the dictionary. 
-            Otherwise nothing has to be done. 
-        """
-        if vertex not in self.__graph_dict:
-            self.__graph_dict[vertex] = set()
+        if vertex not in self.graph_dict:
+            self.graph_dict[vertex] = set()
 
     def add_edge(self, edge):
-        """ assumes that edge is of type set, tuple or list; 
-            between two vertices can be multiple edges! 
-        """
         edge = set(edge)
         (vertex1, vertex2) = tuple(edge)
-        self.__graph_dict[vertex1].add(vertex2)
+        self.graph_dict[vertex1].add(vertex2)
 
     def symetrize(self):
         for i, item in enumerate(self.__generate_edges()):
             if len(item) != 1:
                 (vertex1, vertex2) = tuple(item)
-                if type(self.__graph_dict[vertex1]) is dict:
-                    self.__graph_dict[vertex1] = {vertex2}
+                if type(self.graph_dict[vertex1]) is dict:
+                    self.graph_dict[vertex1] = {vertex2}
                 else:
-                    self.__graph_dict[vertex1].add(vertex2)
-                if type(self.__graph_dict[vertex2]) is dict:
-                    self.__graph_dict[vertex2] = {vertex1}
+                    self.graph_dict[vertex1].add(vertex2)
+                if type(self.graph_dict[vertex2]) is dict:
+                    self.graph_dict[vertex2] = {vertex1}
                 else:
-                    self.__graph_dict[vertex2].add(vertex1)
+                    self.graph_dict[vertex2].add(vertex1)
 
     def remove_self_loops(self):
-        vertices = self.vertices()
+        vertices = self.get_vertices()
         for i, item in enumerate(vertices):
-            if type(self.__graph_dict[item]) is not dict:
-                self.__graph_dict[item] = self.__graph_dict[item].difference({item})
+            if type(self.graph_dict[item]) is not dict:
+                self.graph_dict[item] = self.graph_dict[item].difference({item})
 
     def remove_vertexes_of_degree_1(self):
         deleted = set()
         change = True
         while change:
             change = False
-            for i, item in enumerate(list(self.__graph_dict.keys())):
-                if type(self.__graph_dict[item]) is not dict:
-                    temp = self.__graph_dict[item].difference(deleted)
-                    if temp != self.__graph_dict[item]:
-                        self.__graph_dict[item] = temp
+            for i, item in enumerate(list(self.graph_dict.keys())):
+                if type(self.graph_dict[item]) is not dict:
+                    temp = self.graph_dict[item].difference(deleted)
+                    if temp != self.graph_dict[item]:
+                        self.graph_dict[item] = temp
                         change = True
-                if len(self.__graph_dict[item]) == 1:
+                if len(self.graph_dict[item]) == 1:
                     deleted = deleted.union(item)
-                    del self.__graph_dict[item]
+                    del self.graph_dict[item]
                     change = True
 
-    def graph_dict(self):
-        return self.__graph_dict
-
-    def get_Adj(self, vertex):
-        return self.__graph_dict[vertex]
-
     def __generate_edges(self):
-        """ A static method generating the edges of the 
-            graph "graph". Edges are represented as sets 
-            with one (a loop back to the vertex) or two 
-            vertices 
-        """
         edges = []
-        for vertex in self.__graph_dict:
-            for neighbour in self.__graph_dict[vertex]:
+        for vertex in self.graph_dict:
+            for neighbour in self.graph_dict[vertex]:
                 if {neighbour, vertex} not in edges:
                     edges.append({vertex, neighbour})
         return edges
 
     def __str__(self):
         res = "vertices: "
-        for k in self.__graph_dict:
+        for k in self.graph_dict:
             res += str(k) + " "
         res += "\nedges: "
         for edge in self.__generate_edges():
@@ -114,33 +90,7 @@ if __name__ == "__main__":
     graph = Graph(g)
 
     print(graph.graph_dict())
+
     graph.symetrize()
-    #print("Vertices of graph:")
-    #print(graph.vertices())
 
-    #print("Edges of graph:")
     print(graph.graph_dict())
-
-    #print("Add vertex:")
-    #graph.add_vertex("z")
-
-    #print("Vertices of graph:")
-    #print(graph.vertices())
- 
-    #print("Add an edge:")
-    #graph.add_edge({"a","z"})
-    #graph.add_edge({"a","z"})
-    #graph.add_edge({"a","z"})
-    #
-    #print("Vertices of graph:")
-    #print(graph.vertices())
-
-    #print("Edges of graph:")
-    #print(graph.edges())
-
-    #print('Adding an edge {"x","y"} with new vertices:')
-    #graph.add_edge({"x","y"})
-    #print("Vertices of graph:")
-    #print(graph.vertices())
-    #print("Edges of graph:")
-    #print(graph.edges())
--- 3_semestr/GAL/Proj1/main.py
+++ 3_semestr/GAL/Proj1/main.py
@@ -2,11 +2,12 @@ from collections import deque
 import random
 
 from graph import Graph
+from graphs import g_1, g_2, g_3, g_4
 from utils import draw_graph
 
 def isPlanar(graph):
-    V_count = len(graph.vertices())
-    E_count = len(graph.edges())
+    V_count = len(graph.get_vertices())
+    E_count = len(graph.get_edges())
 
     if E_count > 3 * V_count - 6:
         return False
@@ -14,7 +15,7 @@ def isPlanar(graph):
     return True
 
 def get_disconnected_components(graph):
-    vertices = set(graph.vertices())
+    vertices = set(graph.get_vertices())
     components = []
     while vertices:
         s = random.choice(list(vertices))
@@ -43,11 +44,9 @@ def get_disconnected_components(graph):
 
     graphs = []
     for comp in components:
-        graphs.append(Graph(dict((k, graph.graph_dict()[k]) for k in list(comp) if k in graph.graph_dict())))
+        graphs.append(Graph(dict((k, graph.get_graph_dict()[k]) for k in list(comp) if k in graph.get_graph_dict())))
 
     return graphs
-
-
 
 def DFS(graph):
     def cycle(u):
@@ -63,10 +62,10 @@ def DFS(graph):
     DF_count = 0
     D = dict()
     a = dict()
-    for i, item in enumerate(graph.vertices()):
+    for i, item in enumerate(graph.get_vertices()):
         D[item] = 0
     
-    u = random.choice(list(graph.vertices()))
+    u = random.choice(list(graph.get_vertices()))
 
     cycle(u)
     print(D)
@@ -74,89 +73,18 @@ def DFS(graph):
 
 
 if __name__ == "__main__":
-    g_1 = {"a" : {"e", "b"},
-           "b" : {"a", "e", "f", "c"},
-           "c" : {"b", "g", "h"},
-           "d" : {"e", "l"},
-           "e" : {"a", "b", "i", "d"},
-           "f" : {"b", "g"},
-           "g" : {"c", "f", "j"},
-           "h" : {"c", "k", "o"},
-           "i" : {"e", "j", "l"},
-           "j" : {"i", "g", "k"},
-           "k" : {"j", "h", "o", "m"},
-           "l" : {"d", "i", "m"},
-           "m" : {"l", "k", "n"},
-           "n" : {"m", "o"},
-           "o" : {"h", "k", "n"}
-          }
 
-    g_2 = {"a" : {"e", "b"},
-           "b" : {"a", "e", "f", "c"},
-           "c" : {"b", "g", "h"},
-           "d" : {"e", "l"},
-           "e" : {"a", "b", "i", "d"},
-           "f" : {"b", "g", "k"},
-           "g" : {"c", "f", "j"},
-           "h" : {"c", "k", "o"},
-           "i" : {"e", "j", "l"},
-           "j" : {"i", "g", "k"},
-           "k" : {"j", "h", "o", "m", "f"},
-           "l" : {"d", "i", "m"},
-           "m" : {"l", "k", "n"},
-           "n" : {"m", "o"},
-           "o" : {"h", "k", "n"}
-          }
-
-    g_3 = {"a" : {"e", "b"},
-           "b" : {"a", "e", "f", "c"},
-           "c" : {"b", "g"},
-           "d" : {"l"},
-           "e" : {"a", "b"},
-           "f" : {"b", "g"},
-           "g" : {"c", "f"},
-           "h" : {"k", "o"},
-           "i" : {"j", "l"},
-           "j" : {"i", "k"},
-           "k" : {"j", "h", "o", "m"},
-           "l" : {"d", "i", "m"},
-           "m" : {"l", "k", "n"},
-           "n" : {"m", "o"},
-           "o" : {"h", "k", "n"}
-          }
-
-    g_4 = {"a" : {"b"},
-           "b" : {"a", "c", "d"},
-           "c" : {"b"},
-           "d" : {"b", "d"},
-           "e" : {}
-          }
 
     planar = Graph(g_1)
     non_planar = Graph(g_2)
     more_components = Graph(g_3)
     self_loop = Graph(g_4)
 
-    print(isPlanar(planar))
-    print(isPlanar(non_planar))
-
-                    #remove self loops
-                    #remove vertexes of degree 1
+    planar.remove_self_loops()
+    planar.remove_vertexes_of_degree_1()
     graphs = get_disconnected_components(planar) # get disconnected components
 
     for i, item in enumerate(graphs):
         print(item)
 
-    print(self_loop)
-    self_loop.remove_self_loops()
-    print(self_loop)
-    self_loop.remove_vertexes_of_degree_1()
-    print(self_loop)
-    graphs = get_disconnected_components(self_loop)
-    for i, item in enumerate(graphs):
-        print(item)
-
-    print("####")
     DFS(planar)
-
-    draw_graph(planar)
