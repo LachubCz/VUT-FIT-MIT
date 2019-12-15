c4b8ab829becb999c548e3414dbfa83ded621327
LachubCz <petr.genius.buchal@gmail.com>
2019-12-15 12:22:34 +0100

    bicomponents partition added

--- 3_semestr/GAL/Proj1/main.py
+++ 3_semestr/GAL/Proj1/main.py
@@ -1,10 +1,11 @@
-from collections import deque
 import random
 random.seed(0)
+from collections import deque
+from operator import itemgetter
 
 from graph import Graph
-#from graphs import g_1, g_2, g_3, g_4
 from utils import draw_graph
+#from graphs import g_1, g_2, g_3, g_4
 
 def isPlanar(graph):
     V_count = len(graph.get_vertices())
@@ -12,6 +13,9 @@ def isPlanar(graph):
 
     if E_count > 3 * V_count - 6:
         return False
+
+    if V_count < 5:
+        return True
 
     return True
 
@@ -49,30 +53,50 @@ def get_disconnected_components(graph):
 
     return graphs
 
-from operator import itemgetter
 
 def DFS(graph):
-    def cycle(u):
+    def recursion(u):
         global DF_count
         DF_count += 1
         D[u] = DF_count
-        for _, v in enumerate(graph.get_Adj(u)):
+        children = 0
+        low[u] = DF_count
+        
+        for i, v in enumerate(graph.get_Adj(u)):
             if D[v] == 0:
                 a[v] = u
-                cycle(v)
+                children += 1
+                recursion(v)
+                low[u] = min(low[u], low[v]) 
+                if a[u] == None and children > 1: 
+                    ap[u] = True
+
+                if a[u] != None and low[v] >= D[u]: 
+                    ap[u] = True 
+
+            elif v != a[u]:
+                low[u] = min(low[u], D[v])
 
     global DF_count
     DF_count = 0
-    D = dict()
-    a = dict()
+
+    D        = dict()
+    a        = dict()
+    low      = dict()
+    ap       = dict()
     for i, item in enumerate(graph.get_vertices()):
         D[item] = 0
+        a[item] = None
+        low[item] = float("Inf")
+        ap[item] = False
     
     u = random.choice(list(graph.get_vertices()))
+    recursion(u)
 
-    cycle(u)
-
-    print(D, a)
+    print("Order: {}" .format(D))
+    print("Parents: {}" .format(a))
+    print("Articulation points: {}" .format(ap))
+    print("Lowpoints: {}" .format(low))
 
     order = dict((v,k) for k,v in D.items()) #swap keys with values
 
@@ -89,7 +113,10 @@ def DFS(graph):
 
         Adjs = Adjs.difference({parent})
 
-        back = list(itemgetter(*frozenset(Adjs))(D))
+        if len(Adjs) != 1:
+            back = list(itemgetter(*frozenset(Adjs))(D))
+        else:
+            back = [D[list(Adjs)[0]]]
         back.append(i)
 
         if order[i] in L1:
@@ -109,66 +136,85 @@ def DFS(graph):
         else:
             L2[order[i]] = D[order[2]]
 
-    print(L1)
-    print(L2)
+    print("Lowpoints: {}" .format(L1))
+    print("Lowpoints: {}" .format(L2))
 
-def DFS2(graph):
-    def cycle(v):
-        global pocet
-        global S
-        pocet += 1
-        d[v] = pocet
-        S = S.union({v})
-        low[v] = d[v]
-        komp.append(v)
+    print(get_biconnected_components(graph, ap))
 
-        for _, w in enumerate(graph.get_Adj(v)):
-            if w not in S:
-                p[w] = v
-                cycle(w)
-                low[v] = min([low[v], low[v]])
-            else:
-                low[v] = min([low[v], d[v]])
-        if d[v] >= 2 and low[v] == d[p[v]]:
-            while True:
-                u = komp.pop()
-                if u == v:
-                    break
+def get_biconnected_components(graph, ap):
+    graph_dict = graph.get_graph_dict()
+    for i, item in enumerate(ap.keys()):
+        if ap[item]:
+            del graph_dict[item]
 
-    global pocet
-    global S
-    pocet = 0
-    d = dict()
-    S = set()
-    low = dict()
-    p = dict()
-    komp = deque()
-    
-    u = random.choice(list(graph.get_vertices()))
+    components = []
+    vertices = set(graph.get_vertices())
 
-    cycle(u)
-    print(d)
-    print(p)
-    print(low)
+    while vertices:
+        s = random.choice(list(vertices))
+
+        d = set()
+        color = dict()
+        
+        for i, item in enumerate(list(vertices)):
+            color[item] = "white"
+
+        color[s] = "gray"
+        Q = deque()
+        Q.append(s)
+        
+        while Q:
+            u = Q.popleft() 
+            for _, v in enumerate(graph.get_Adj(u)):
+                if v in graph_dict:
+                    if color[v] == "white":
+                        color[v] = "gray"
+                        Q.append(v)
+            color[u] = "black"
+            d.add(u)
+
+        components.append(d)
+        vertices = vertices - d
+
+    graphs = []
+    for comp in components:
+        graphs.append(Graph(dict((k, graph.get_graph_dict()[k]) for k in list(comp) if k in graph.get_graph_dict())))
+
+    return graphs
+
 
 if __name__ == "__main__":
-    g = {"a" : {"f", "h", "b"},
-         "b" : {"c", "a", "g"},
-         "c" : {"h", "b", "d"},
-         "h" : {"a", "c", "e"},
-         "e" : {"d", "h", "f"},
-         "f" : {"e", "g", "a"},
-         "g" : {"b", "d", "f"},
-         "d" : {"g", "e", "c"}
+    #g = {"a" : {"f", "h", "b"},
+    #     "b" : {"c", "a", "g"},
+    #     "c" : {"h", "b", "d"},
+    #     "h" : {"a", "c", "e"},
+    #     "e" : {"d", "h", "f"},
+    #     "f" : {"e", "g", "a"},
+    #     "g" : {"b", "d", "f"},
+    #     "d" : {"g", "e", "c"}
+    #    }
+    
+    g = {"a" : {"b", "d"}, 
+         "b" : {"a", "c"}, 
+         "c" : {"b", "d", "e", "f"}, 
+         "d" : {"a", "c"}, 
+         "e" : {"c", "f"}, 
+         "f" : {"c", "e"}
         }
+
     graph = Graph(g)
 
-    graph.remove_self_loops()
-    graph.remove_vertexes_of_degree_1()
-    graphs = get_disconnected_components(graph)
+    #print(graph)
+    #graph.symetrize()
+    #print(graph)
+
+    #graph.remove_self_loops()
+    #graph.remove_vertexes_of_degree_1()
+    #graphs = get_disconnected_components(graph)
 
     #for i, item in enumerate(graphs):
     #    print(item)
 
     #DFS2(graph)
     DFS(graph)
+    #GetArticulationPoints(graph)
